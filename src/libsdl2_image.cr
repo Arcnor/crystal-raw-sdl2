980ba2b671273bfe0b1097f915c6b69a47e1c8c6
Edu Garcia <arcnorj@gmail.com>
2015-04-25 13:21:02 +1000

    SDL2_ttf and SDL2_image names are now in snake_case

--- src/libsdl2_image.cr
+++ src/libsdl2_image.cr
@@ -7,59 +7,59 @@ lib LibSDL2_image
 	# Types & Classes #
 	
 	@[Flags]
-	enum IMG_InitFlags
-		IMG_INIT_JPG = 1,
-		IMG_INIT_PNG = 2,
-		IMG_INIT_TIF = 4,
-		IMG_INIT_WEBP = 8
+	enum InitFlags
+		INIT_JPG = 0x00000001,
+		INIT_PNG = 0x00000002,
+		INIT_TIF = 0x00000004,
+		INIT_WEBP = 0x00000008
 	end
-	
-	
+
 	# Functions #
 	
-	fun linked_Version = IMG_Linked_Version() : LibSDL2::SDL_version*
+	fun linked_version = IMG_Linked_Version() : LibSDL2::SDL_version*
 	fun init = IMG_Init(flags : Int32) : Int32
 	fun quit = IMG_Quit() : Void
-	fun loadTyped_RW = IMG_LoadTyped_RW(src : LibSDL2::SDL_RWops*, freesrc : Int32, type : UInt8*) : LibSDL2::SDL_Surface*
+	fun load_typed_rw = IMG_LoadTyped_RW(src : LibSDL2::SDL_RWops*, freesrc : Int32, type : UInt8*) : LibSDL2::SDL_Surface*
 	fun load = IMG_Load(file : UInt8*) : LibSDL2::SDL_Surface*
-	fun load_RW = IMG_Load_RW(src : LibSDL2::SDL_RWops*, freesrc : Int32) : LibSDL2::SDL_Surface*
-	fun loadTexture = IMG_LoadTexture(renderer : LibSDL2::SDL_Renderer*, file : UInt8*) : LibSDL2::SDL_Texture*
-	fun loadTexture_RW = IMG_LoadTexture_RW(renderer : LibSDL2::SDL_Renderer*, src : LibSDL2::SDL_RWops*, freesrc : Int32) : LibSDL2::SDL_Texture*
-	fun loadTextureTyped_RW = IMG_LoadTextureTyped_RW(renderer : LibSDL2::SDL_Renderer*, src : LibSDL2::SDL_RWops*, freesrc : Int32, type : UInt8*) : LibSDL2::SDL_Texture*
-	fun isICO = IMG_isICO(src : LibSDL2::SDL_RWops*) : Int32
-	fun isCUR = IMG_isCUR(src : LibSDL2::SDL_RWops*) : Int32
-	fun isBMP = IMG_isBMP(src : LibSDL2::SDL_RWops*) : Int32
-	fun isGIF = IMG_isGIF(src : LibSDL2::SDL_RWops*) : Int32
-	fun isJPG = IMG_isJPG(src : LibSDL2::SDL_RWops*) : Int32
-	fun isLBM = IMG_isLBM(src : LibSDL2::SDL_RWops*) : Int32
-	fun isPCX = IMG_isPCX(src : LibSDL2::SDL_RWops*) : Int32
-	fun isPNG = IMG_isPNG(src : LibSDL2::SDL_RWops*) : Int32
-	fun isPNM = IMG_isPNM(src : LibSDL2::SDL_RWops*) : Int32
-	fun isTIF = IMG_isTIF(src : LibSDL2::SDL_RWops*) : Int32
-	fun isXCF = IMG_isXCF(src : LibSDL2::SDL_RWops*) : Int32
-	fun isXPM = IMG_isXPM(src : LibSDL2::SDL_RWops*) : Int32
-	fun isXV = IMG_isXV(src : LibSDL2::SDL_RWops*) : Int32
-	fun isWEBP = IMG_isWEBP(src : LibSDL2::SDL_RWops*) : Int32
-	fun loadICO_RW = IMG_LoadICO_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadCUR_RW = IMG_LoadCUR_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadBMP_RW = IMG_LoadBMP_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadGIF_RW = IMG_LoadGIF_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadJPG_RW = IMG_LoadJPG_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadLBM_RW = IMG_LoadLBM_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadPCX_RW = IMG_LoadPCX_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadPNG_RW = IMG_LoadPNG_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadPNM_RW = IMG_LoadPNM_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadTGA_RW = IMG_LoadTGA_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadTIF_RW = IMG_LoadTIF_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadXCF_RW = IMG_LoadXCF_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadXPM_RW = IMG_LoadXPM_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadXV_RW = IMG_LoadXV_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun loadWEBP_RW = IMG_LoadWEBP_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
-	fun readXPMFromArray = IMG_ReadXPMFromArray(xpm : UInt8**) : LibSDL2::SDL_Surface*
-	fun savePNG = IMG_SavePNG(surface : LibSDL2::SDL_Surface*, file : UInt8*) : Int32
-	fun savePNG_RW = IMG_SavePNG_RW(surface : LibSDL2::SDL_Surface*, dst : LibSDL2::SDL_RWops*, freedst : Int32) : Int32
+	fun load_rw = IMG_Load_RW(src : LibSDL2::SDL_RWops*, freesrc : Int32) : LibSDL2::SDL_Surface*
+	fun load_texture = IMG_LoadTexture(renderer : LibSDL2::SDL_Renderer*, file : UInt8*) : LibSDL2::SDL_Texture*
+	fun load_texture_rw = IMG_LoadTexture_RW(renderer : LibSDL2::SDL_Renderer*, src : LibSDL2::SDL_RWops*, freesrc : Int32) : LibSDL2::SDL_Texture*
+	fun load_texture_typed_rw = IMG_LoadTextureTyped_RW(renderer : LibSDL2::SDL_Renderer*, src : LibSDL2::SDL_RWops*, freesrc : Int32, type : UInt8*) : LibSDL2::SDL_Texture*
+	fun is_ico = IMG_isICO(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_cur = IMG_isCUR(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_bmp = IMG_isBMP(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_gif = IMG_isGIF(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_jpg = IMG_isJPG(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_lbm = IMG_isLBM(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_pcx = IMG_isPCX(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_png = IMG_isPNG(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_pnm = IMG_isPNM(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_tif = IMG_isTIF(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_xcf = IMG_isXCF(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_xpm = IMG_isXPM(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_xv = IMG_isXV(src : LibSDL2::SDL_RWops*) : Int32
+	fun is_webp = IMG_isWEBP(src : LibSDL2::SDL_RWops*) : Int32
+	fun load_ico_rw = IMG_LoadICO_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_cur_rw = IMG_LoadCUR_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_bmp_rw = IMG_LoadBMP_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_gif_rw = IMG_LoadGIF_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_jpg_rw = IMG_LoadJPG_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_lbm_rw = IMG_LoadLBM_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_pcx_rw = IMG_LoadPCX_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_png_rw = IMG_LoadPNG_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_pnm_rw = IMG_LoadPNM_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_tga_rw = IMG_LoadTGA_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_tif_rw = IMG_LoadTIF_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_xcf_rw = IMG_LoadXCF_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_xpm_rw = IMG_LoadXPM_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_xv_rw = IMG_LoadXV_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun load_webp_rw = IMG_LoadWEBP_RW(src : LibSDL2::SDL_RWops*) : LibSDL2::SDL_Surface*
+	fun read_xpm_from_array = IMG_ReadXPMFromArray(xpm : UInt8**) : LibSDL2::SDL_Surface*
+	fun save_png = IMG_SavePNG(surface : LibSDL2::SDL_Surface*, file : UInt8*) : Int32
+	fun save_png_rw = IMG_SavePNG_RW(surface : LibSDL2::SDL_Surface*, dst : LibSDL2::SDL_RWops*, freedst : Int32) : Int32
 	
 	# Vars #
 	
 	
 end
+
